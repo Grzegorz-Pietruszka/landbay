14495d3aa51e269396c6cc06f721300dd2fa8c2f
Grzegorz Pietruszka <pietruszka.grzegorz@gmail.com>
2019-05-21 13:55:43 +0100

    validation service return only boolean values

--- src/app/common/validators/validation.service.ts
+++ src/app/common/validators/validation.service.ts
@@ -1,59 +1,30 @@
 import { Injectable } from '@angular/core';
-import { ValidationErrors, AbstractControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormGroup } from '@angular/forms';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ValidationService {
-    static getValidatorMessage(validatorName: string) {
-        let config = {
-            invalidEmail: 'Email address is invalid',
-            invalidConfirmEmail: `Emails don't match`,
-            invalidPassword:
-                'Password must be 10 characters long and contains at least lover case letter, uppercase letter, number, special character',
-            invalidPin: 'PIN is invalid',
-        };
-        return config[validatorName];
+    static emailValidator(control: AbstractControl): boolean {
+        const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return regex.test(control.value);
     }
 
-    static emailValidator(control: AbstractControl): object | null {
-        if (
-            control.value.match(
-                /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
-            )
-        ) {
-            return null;
-        } else {
-            return { invalidEmailAddress: true };
-        }
-    }
-
-    static MatchValues(controlName: string, matchingControlName: string): object | null {
+    static matchValues(controlName: string, matchingControlName: string) {
         return (formGroup: FormGroup) => {
             const control = formGroup.controls[controlName];
             const matchingControl = formGroup.controls[matchingControlName];
-            if (control.value === matchingControl.value) {
-                return null;
-            } else {
-                return { notMatch: true };
-            }
+            return control.value === matchingControl.value;
         };
     }
 
-    static correctEmail(control: AbstractControl): object | null {
+    static correctEmail(control: AbstractControl): boolean {
         let allowedEmails = ['john@investor.com'];
-        if (!allowedEmails.includes(control.value as string) && (control.value as string) !== '') {
-            return { isNotAllowed: true };
-        } else {
-            return null;
-        }
+        return !allowedEmails.includes(control.value as string) && (control.value as string) !== '';
     }
 
-    static passwordValidator(control: AbstractControl): object | null {
-        if (control.value.match(/^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,100}$/)) {
-            return null;
-        } else {
-            return { invalidPassword: true };
-        }
+    static passwordValidator(control: AbstractControl): boolean {
+        const regex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{10,}$/;
+        return regex.test(control.value);
     }
 }
